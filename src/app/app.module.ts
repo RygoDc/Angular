95e45d99a466162988a864cf6df9b98e8bda3d5b
Rodrigo <rdc.1985@gmail.com>
2024-11-06 09:48:06 +0100

    actualizado primerProyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RepasoComponent } from './repaso/repaso.component';
+import { FormComponent } from './form/form.component';
+import { TarjetaComponent } from './tarjeta/tarjeta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    RepasoComponent
+    RepasoComponent,
+    FormComponent,
+    TarjetaComponent
   ],
   imports: [
     BrowserModule,
